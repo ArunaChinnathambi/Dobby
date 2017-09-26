345f4011c8a1401b6ae7b6941467ad524b7d4f3b
Aruna Chinnathambi <aruna.c@KGFSL.COM>
2017-09-26 14:49:39 +0530

    Single Page CRUD

--- AngularJS Experiments/Light.js
+++ AngularJS Experiments/Light.js
@@ -2,25 +2,25 @@ var List = [];
 var app = angular.module("App", []);
 var key = 0;
 
-app.controller("Ctrl", function($scope) {
+app.controller("Ctrl", function ($scope) {
   $scope.users = List;
   $scope.Add = true;
   $scope.Update = false;
-  $scope.add = function() {
+  $scope.add = function () {
     var val = $scope.name;
     $scope.name = "";
     List.push(val);
   };
-  $scope.edit = function(x) {
+  $scope.edit = function (x) {
     $scope.name = List[x];
     key = x;
     $scope.Add = false;
     $scope.Update = true;
   };
-  $scope.delete = function(x) {
+  $scope.delete = function (x) {
     List.splice(x, 1);
   };
-  $scope.update = function() {
+  $scope.update = function () {
     List[key] = $scope.name;
     $scope.name = "";
     $scope.Add = true;
@@ -40,7 +40,7 @@ app.controller("Ctrl", function($scope) {
 //   $scope.add = function() {
 //     $scope.users.push($scope.Note);
 //     $scope.Note = {};
-    
+
 //   };
 //   $scope.edit = function(x) {
 //     $scope.Note = List[x];
